ded84257744ec6dbefc5357a554726de1a8117b3
Mario Menjivar <mariomenjr@gmail.com>
2024-07-21 00:14:22 -0700

    Improve comment in default.ts

--- src/core/utils/default.ts
+++ src/core/utils/default.ts
@@ -1,17 +1,56 @@
 import * as fabric from 'fabric';
 
 /**
+ * The default style for the selection controls of newly created objects.
+ */
+const selectionStyle = {
+    /**
+     * The color of the selection border.
+     */
+    borderColor: 'gray',
+    /**
+     * The color of the selection corners.
+     */
+    cornerColor: 'gray',
+    /**
+     * The size of the selection corners.
+     */
+    cornerSize: 8,
+    /**
+     * Whether the selection corners are transparent.
+     */
+    transparentCorners: true
+};
+
+/**
  * Creates a new fabric Rectangle object with default properties.
  *
  * @returns {fabric.Rect} The created rectangle object.
  */
 export function createRectangle(): fabric.Rect {
     return new fabric.Rect({
-        left: 100, // x-coordinate of the rectangle's leftmost point
-        top: 100, // y-coordinate of the rectangle's topmost point
-        fill: 'red', // color of the rectangle
-        width: 20, // width of the rectangle
-        height: 20, // height of the rectangle
+        /**
+         * The x-coordinate of the rectangle's leftmost point.
+         */
+        left: 100,
+        /**
+         * The y-coordinate of the rectangle's topmost point.
+         */
+        top: 100,
+        /**
+         * The color of the rectangle.
+         */
+        fill: 'red',
+        /**
+         * The width of the rectangle.
+         */
+        width: 20,
+        /**
+         * The height of the rectangle.
+         */
+        height: 20,
+
+        ...selectionStyle
     });
 }
 
@@ -23,10 +62,24 @@ export function createRectangle(): fabric.Rect {
  */
 export function createTextbox(text: string = `New Text`): fabric.Textbox {
     return new fabric.Textbox(text, {
-        left: 50, // x-coordinate of the textbox's leftmost point
-        top: 50, // y-coordinate of the textbox's topmost point
-        width: 150, // width of the textbox
-        fontSize: 20, // font size of the textbox
+        /**
+         * The x-coordinate of the textbox's leftmost point.
+         */
+        left: 50,
+        /**
+         * The y-coordinate of the textbox's topmost point.
+         */
+        top: 50,
+        /**
+         * The width of the textbox.
+         */
+        width: 150,
+        /**
+         * The font size of the textbox.
+         */
+        fontSize: 20,
+
+        ...selectionStyle,
     });
 }
 
