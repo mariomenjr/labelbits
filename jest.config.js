e20b42228235fd2759bf80b3c38264e321436ff7
Mario Menjivar <mariomenjr@gmail.com>
2024-08-03 18:33:43 -0700

    Setup Jest for tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      }
+    ],
+  },
+};
---
+++ tests/utils/strings.test.ts
@@ -0,0 +1,19 @@
+import { camelToTitleCase, camelToKebabCase } from "../../src/utils/strings";
+
+describe("camelToTitleCase", () => {
+    it("should convert a camel case string to title case", () => {
+        expect(camelToTitleCase("helloWorld")).toBe("Hello World");
+        expect(camelToTitleCase("camelCase")).toBe("Camel Case");
+        expect(camelToTitleCase("")).toBe("");
+        expect(camelToTitleCase("hello123")).toBe("Hello123");
+    });
+});
+
+describe("camelToKebabCase", () => {
+    it("should convert a camel case string to kebab case", () => {
+        expect(camelToKebabCase("helloWorld")).toBe("hello-world");
+        expect(camelToKebabCase("camelCase")).toBe("camel-case");
+        expect(camelToKebabCase("")).toBe("");
+        expect(camelToKebabCase("hello123")).toBe("hello123");
+    });
+})
