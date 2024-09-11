094b0232be72bfa13e322e4080daff8bfdc3d82b
Mario Menjivar <mariomenjr@gmail.com>
2024-09-10 23:13:58 -0700

    Fix jest config for aliases

--- packages/jest.config/jest.config.js
+++ packages/jest.config/jest.config.js
@@ -5,8 +5,14 @@ module.exports = {
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
-    '^@labelbits/designer-([^/]+)(?:/([^/]+))?$': '<rootDir>/../../designer/$1/src',
-    '^@labelbits/designer-([^/]+)(?:/([^/]+))?$': '<rootDir>/../../designer/$1/src/$2',
+    // Matches patterns like '@labelbits/designer-shared'
+    '^@labelbits/designer-(\\w+)$': '<rootDir>/../../designer/$1/src',
+
+    // Matches patterns like '@labelbits/designer-core/fabric'
+    '^@labelbits/designer-(\\w+)/(\\w+)$': '<rootDir>/../../designer/$1/src/$2',
+
+    // Matches patterns like '@labelbits/designer-plugin-barcode'
+    '^@labelbits/designer-(\\w+)-(\\w+)$': '<rootDir>/../../designer/$1/$2/src',
   },
   setupFilesAfterEnv: ['<rootDir>/../../packages/jest.config/jest.setup.js'],
   transform: {
