73389be4a5e15e1eb4d5d1565606aa6ec9c60ff4
Mario Menjivar <mariomenjr@gmail.com>
2024-10-19 23:18:17 -0700

    Setup loading dots

--- designer/app/src/index.ts
+++ designer/app/src/index.ts
@@ -1,11 +1,53 @@
 import App from "./app/App"; // Import the App class from the app module
 
 /**
+ * Hides the loading screen and displays the main application.
+ *
+ * Removes the element with the id "loading" and unhides the elements with the
+ * names "main" and "header". Also removes the element with the name "style".
+ *
+ * @private
+ */
+function endLoading() {
+
+    const tags = [
+        {
+            name: `main`,
+        },
+        {
+            name: `header`,
+        },
+        {
+            name: `style`,
+        }
+    ];
+
+    for (const tag of tags) {
+
+        const elements = document.getElementsByTagName(tag.name);
+        for (const el of elements) {
+
+            switch (tag.name) {    
+                case `style`:
+                    el.remove();
+                    break;
+                        
+                default:
+                    el.removeAttribute("hidden");
+                    break;
+            }
+        }
+    }
+
+    document.getElementById("loading")!.remove();
+}
+
+/**
  * Starts the application when the page is loaded.
  * The application is started by calling the static {@link App.start} method.
  */
-window.onload = function() {
-    // Start the application when the page is loaded
+window.onload = function () {
+
+    endLoading();
     App.start();
 };
-
